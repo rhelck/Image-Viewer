9ff4ae3d3a559fb93172a9bcb7f4955a5d2cb25b
rhelck <64742197+rhelck@users.noreply.github.com>
2020-08-18 12:14:59 -0400

    Add files via upload

---
+++ image_view_helck_style_modular.py
@@ -0,0 +1,93 @@
+from tkinter import *
+import time
+from time import strftime
+from PIL import ImageTk,Image
+from random import *
+import os
+
+root = Tk()
+root.iconbitmap('C:\\Users\\User\\desktop_clock_folder\\favicon.ico')
+
+path = 'C:\\Users\\User\\desktop_clock_folder\\image viewer pics'
+directory = os.listdir(path)
+image_list = []
+image_name_list = []
+
+for f in directory:
+    f_path = path + '\\' + f
+    image_name_list.append(f)
+    image_list.append(ImageTk.PhotoImage(Image.open(f_path)))
+print(image_list)
+print(image_name_list)
+
+position = 1
+
+img_label = Label(root, image = image_list[position])
+img_label.grid(column = 0, row = 0, columnspan = 3)
+#name_label = Label(root, text = image_name_list[position])
+#name_label.grid(column = 0, row = 3, columnspan = 3)
+
+def forward():
+    
+    global img_label
+    global position 
+    global name_label
+
+    if position < (len(image_list) - 1):
+        position = position + 1
+        img_label.grid_forget()
+        img_label = Label(image = image_list[position])
+        img_label.grid(column = 0, row = 0, columnspan = 3)
+        name_label.grid_forget()
+        name_label = Label(text = image_name_list[position])
+        name_label.grid(column = 0, row = 3, columnspan = 3)
+
+def backward():
+
+    global img_label
+    global position
+    global name_label
+
+    if position > 0:
+        position = position - 1
+        img_label.grid_forget()
+        img_label = Label(image = image_list[position])
+        img_label.grid(column = 0, row = 0, columnspan = 3)
+        name_label.grid_forget()
+        name_label = Label(text = image_name_list[position])
+        name_label.grid(column = 0, row = 3, columnspan = 3)
+
+def random_func():
+
+    global img_label
+    global position
+    global name_label
+
+    position = randint(0, (len(image_list)-1))
+    img_label.grid_forget()
+    img_label = Label(image = image_list[position])
+    img_label.grid(column = 0, row = 0, columnspan = 3)
+    name_label.grid_forget()
+    name_label = Label(text = image_name_list[position])
+    name_label.grid(column = 0, row = 3, columnspan = 3)
+
+name_label = Label(root, width = 10, text = '')
+name_label.grid(column = 0, row = 3, columnspan = 3)
+time_label = Label(root, width = 10)
+time_label.grid(column = 0, row = 4, columnspan = 3)
+
+def count():
+    time_now = time.strftime('%H %M %S %p')
+    time_label.configure(text = time_now)
+    time_label.after(1000, count)
+
+count()
+
+back_button = Button(root, text = '<', command = backward)
+back_button.grid(column = 0, row = 1)
+random_button = Button(root, text = 'random', command = random_func)
+random_button.grid(column = 1, row = 1)
+forward_button = Button(root, text = '>', command = forward)
+forward_button.grid(column = 2, row = 1)
+
+root.mainloop()
